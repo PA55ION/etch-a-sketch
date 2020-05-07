53f00d1ed44dc3b4e96dc0a1548b1814a9bb179a
Mai Campbell <jirallc@gmail.com>
2020-05-06 20:25:25 -0400

    feat: change sqaure size from dropdown number

--- app.js
+++ app.js
@@ -3,54 +3,77 @@ let color;
 let sizeOptions = document.getElementById('board-size');
 let cell = document.getElementsByClassName('main-container')[0];
 let div = document.getElementById('container');
-let rainbow = document.getElementsByClassName('random')[0]
+let rainbow = document.getElementById('random');
+let blackBtn = document.getElementById('black');
 let resetBtn = document.getElementById('reset');
-div.addEventListener('mouseover', applyColor)
-rainbow.addEventListener('click', getRandomColors)
-//resetBtn.addEventListener('click', reset)
+const resizeBtn = document.getElementById('resize');
+let square = document.getElementsByClassName('square')[0]
 
-
-function createSquares() {
-    const grid = size * sizeOptions.value
-    for(let i  = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-        const gridSize = div.clientWidth / size;
-        let newSquare = document.createElement('div');
-        newSquare.style.cssText = `width: ${gridSize}px; height: ${gridSize}px`;
-        newSquare.classList.add('square');
-        div.appendChild(newSquare);
-        console.log(gridSize)
+//create  new  square for div container
+function createSquare(size) {
+    let squareSize = div.clientWidth / size;
+    for (let i = 0; i < size; i++) {
+        for (let s = 0; s < size; s++) {
+            let newSquare = document.createElement('div');
+            newSquare.style.cssText = `width: ${squareSize}px; height: ${squareSize}px`;
+            newSquare.classList.add('square');
+            div.appendChild(newSquare)
+            console.log(`width: ${squareSize}px; height: ${squareSize}px`)
         }
     }
-    
 }
+createSquare();
 
-createSquares();
-
-function applyColor(e) {
-    let cell = event.target;
-        // cell.style.background = getRandomColors();
-        switch (color) {
-            case 'black': 
-            cell.style.background = 'black';
-            break;
-            case 'color': 
-            cell.style.background = getRandomColors();
-            break;
-            default:
-                cell.style.background = 'black'
-        }
-}
-
+//get random colors
 function getRandomColors() {
-    let r = Math.floor(Math.random() * 255);
-    let g = Math.floor(Math.random() * 255);
-    let b = Math.floor(Math.random() * 255);
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
 
     return "rgb(" + r + ", " + g + ", " + b + ")";
 }
+//apply colors to square
+function applyColor(e) {
+    let cell = e.target;
 
-function reset() {
+    switch (color) {
+        case 'black':
+            color = 'black'
+            blackBtn.addEventListener('click', () => {
+                cell.style.background = 'black';
+            })
+            break;
+        case 'random':
+            color = getRandomColors();
+            break;
+        default:
+            cell.style.background = 'pink'
+    }
+}
+
+//change square size
+function changeSquareSize() { 
+    let size = Number(sizeOptions.value);
+   
     div.textContent = '';
+    createSquare(size);
+}
+
+changeSquareSize(16);
+
+// function blackColor(e) {
+//     square = e.target;
+//     blackBtn.addEventListener('mouseover', (e) => {
+//         square.style.background = 'black'
+//     })
+// }
+
+//reset div container
+function reset() {
+    div.textContent = ''
     window.location.reload()
 }
+
+div.addEventListener('mouseover', applyColor);
+resize.addEventListener('click', changeSquareSize)
+
